0d18ad6de65073ac135d2aee86fcc59f47edd9af
dbuarque <dio@getty.io>
2019-01-23 14:08:20 -0800

    remove dex files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,10 @@
 /* global artifacts */
 var TWX = artifacts.require('./TWX.sol')
-var TWDex = artifacts.require('./TWDex.sol')
 var OwnableMock = artifacts.require('./OwnableMock.sol')
 var BaseCappedTokenMock = artifacts.require('./BaseCappedTokenMock.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(TWX)
-  deployer.deploy(TWDex)
   deployer.deploy(OwnableMock)
   deployer.deploy(BaseCappedTokenMock)
 }
